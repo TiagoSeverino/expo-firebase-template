4806877734333aaeef5fd8981ec84223b1fc9077
TiagoSeverino <tiago@severino.xyz>
2020-08-02 18:54:09 +0100

    Display User Name and Avatar

--- src/pages/AuthUserProvider.tsx
+++ src/pages/AuthUserProvider.tsx
@@ -1,9 +1,16 @@
 import React, { useState, createContext } from 'react';
+import { User } from 'firebase';
 
-export const AuthUserContext = createContext({});
+export const AuthUserContext = createContext<{
+	user: User | null;
+	setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}>({
+	user: null,
+	setUser: () => {},
+});
 
 export const AuthUserProvider = ({ children }) => {
-	const [user, setUser] = useState(null);
+	const [user, setUser] = useState<User | null>(null);
 
 	return (
 		<AuthUserContext.Provider value={{ user, setUser }}>
