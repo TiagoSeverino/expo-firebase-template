666df4694ace991c95ab233562eca29fd1bc5740
TiagoSeverino <tiago@severino.xyz>
2020-08-31 15:20:27 +0100

    saveExpoPushToken in firestore

--- src/services/Firebase/index.ts
+++ src/services/Firebase/index.ts
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase';
 import 'firebase/auth';
+import 'firebase/firestore';
 import * as Facebook from 'expo-facebook';
 
 import firebaseConfig from './firebaseConfig';
@@ -11,6 +12,7 @@ if (!firebase.apps.length) {
 }
 
 export const auth = firebase.auth();
+export const db = firebase.firestore();
 
 export const loginWithEmail = (email: string, password: string) =>
 	auth.signInWithEmailAndPassword(email, password);
@@ -35,3 +37,46 @@ export const logout = () => auth.signOut();
 
 export const passwordReset = (email: string) =>
 	auth.sendPasswordResetEmail(email);
+
+export const saveExpoPushToken = (token: string | undefined) => {
+	if (!token) return;
+
+	db.collection('private')
+		.doc(auth.currentUser?.uid)
+		.withConverter(privateConverter)
+		.get()
+		.then((doc) => {
+			if (!doc.exists || !doc.data()?.ExpoPushToken?.includes(token)) {
+				db.collection('public')
+					.doc(auth.currentUser?.uid)
+					.set({
+						ExpoPushToken: [
+							...(doc.data()?.ExpoPushToken || []),
+							token,
+						],
+					});
+			}
+		});
+};
+
+interface priv {
+	ExpoPushToken: string[];
+}
+
+// Firestore data converter
+const privateConverter = {
+	toFirestore({ ExpoPushToken }: priv): firebase.firestore.DocumentData {
+		return {
+			ExpoPushToken,
+		};
+	},
+	fromFirestore(
+		snapshot: firebase.firestore.QueryDocumentSnapshot,
+		options: firebase.firestore.SnapshotOptions
+	): priv {
+		const { ExpoPushToken } = snapshot.data(options);
+		return {
+			ExpoPushToken,
+		};
+	},
+};
